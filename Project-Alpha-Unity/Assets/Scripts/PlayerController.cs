40ad45b8893ec11cc450875a8233c8eda65affad
michelYip <wu.michel.yip@gmail.com>
2020-05-26 11:26:04 +0200

    Basic Scene/Player Movement/Folder Hierarchy

---
+++ Project-Alpha-Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 1.0f;
+    public float horizontalMovement = 0.0f;
+    public float verticalMovement = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal") * movementSpeed;
+        verticalMovement = Input.GetAxisRaw("Vertical") * movementSpeed;
+    }
+
+    void FixedUpdate()
+    {
+
+        transform.position += new Vector3(horizontalMovement, 0, verticalMovement) * movementSpeed * Time.deltaTime;
+    }
+}
